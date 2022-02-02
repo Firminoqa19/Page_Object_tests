52fca1937aa9917918f028b87c4bf67e5fdd50f8
Firminoqa19 <firminoqa19@gmail.com>
2022-02-02 21:04:46 +0200

    First commit of Page_Object task

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,11 @@
+from pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
+
 def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
-    browser.get(link)
-    browser.find_element(By.ID, 'login_link').click()
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
